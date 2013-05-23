13ca789e78a7a23469a2eace5c36577e80282b6b
Tracy Wadleigh <tracy.wadleigh@gmail.com>
2013-05-23 03:20:56 -0400

    Initial commit.

---
+++ src/com/github/twadleigh/nav/MainActivity.java
@@ -0,0 +1,106 @@
+package com.github.twadleigh.nav;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements LocationListener, SensorEventListener {
+	
+	private LocationManager mLocationManager;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private Sensor mGyroscope;
+	private Sensor mMagneticField;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // set up location services
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        
+        // set up sensor services
+        mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+    	mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+    	mSensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+    
+    @Override
+    protected void onPause() {
+    	mLocationManager.removeUpdates(this);
+    	mSensorManager.unregisterListener(this);
+    	super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    private TextView txt(int id)
+    {
+    	return (TextView) this.findViewById(id);
+    }
+
+	@Override
+	public void onLocationChanged(Location loc) {
+		// update the location
+		String str = "" + SystemClock.uptimeMillis()*1000000 + "," + loc.getTime() + "," + loc.getLatitude() + "," + loc.getLongitude() + "," + loc.getAltitude();
+		txt(R.id.location_text).setText(str);
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent evt) {
+		String str = "" + evt.timestamp + "," + evt.values[0] + "," + evt.values[1] + "," + evt.values[2];
+		if(evt.sensor == mAccelerometer) {
+			txt(R.id.accelerometer_text).setText(str);
+		} else if(evt.sensor == mGyroscope) {
+			txt(R.id.gyroscope_text).setText(str);
+		} else if(evt.sensor == mMagneticField) {
+			txt(R.id.magnetic_field_text).setText(str);
+		}
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// do nothing
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// do nothing
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// do nothing
+	}
+}
---
+++ src/com/github/twadleigh/nav/Nav.java
@@ -0,0 +1,124 @@
+package com.github.twadleigh.nav;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
+import org.apache.commons.math3.geometry.euclidean.threed.RotationOrder;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+
+import android.location.Location;
+
+public class Nav {
+	
+	// *** static stuff ***
+
+	// WGS84 constants
+	static final public double WGS84_SEMI_MAJOR_AXIS_M = 6378137.0;
+	static final public double WGS84_INVERSE_FLATTENING = 298.257223563;
+	static final public double WGS84_FLATTENING = 1.0/Nav.WGS84_INVERSE_FLATTENING;
+	static final public double WGS84_FIRST_ECCENTRICITY_SQUARED = 
+			1.0-(1.0-Nav.WGS84_FLATTENING)*(1.0-Nav.WGS84_FLATTENING);
+	
+	static public Vector3D geodeticToEcef(double lon_DEG, double lat_DEG, double alt_M) {
+		double lon_RAD = Math.toRadians(lon_DEG);
+		double lat_RAD = Math.toRadians(lat_DEG);
+		double sLon = Math.sin(lon_RAD);
+		double cLon = Math.cos(lon_RAD);
+		double sLat = Math.sin(lat_RAD);
+		double cLat = Math.cos(lat_RAD);
+		
+		double N = Nav.WGS84_SEMI_MAJOR_AXIS_M/
+				Math.sqrt(1.0-Nav.WGS84_FIRST_ECCENTRICITY_SQUARED*sLat*sLat);
+		
+		return new Vector3D(
+				(N+alt_M)*cLat*cLon,
+				(N+alt_M)*cLat*sLon,
+				(N*(1.0-Nav.WGS84_FIRST_ECCENTRICITY_SQUARED)+alt_M)*sLat);
+	}
+	
+	static public Rotation ecefToEnuRotation(double lon_DEG, double lat_DEG, double alt_M) {
+		return new Rotation(
+				RotationOrder.ZXZ, 
+				Math.toRadians(lon_DEG), 
+				Math.toRadians(lat_DEG),
+				0.0);
+	}
+	
+	// *** state ***
+	
+	// origin of ENU frame
+	private Vector3D origin_LLA;
+	
+	// time (nanoseconds since startup)
+	private long t_nS;
+	
+	// position
+	private Vector3D x_M;
+	
+	// velocity
+	private Vector3D v_MpS;
+	
+	// acceleration
+	private Vector3D a_MpS2;
+	
+	// orientation
+	private Rotation theta;
+	
+	// rotation rate
+	private Vector3D omega_RADpS;
+	
+	// gravity
+	private Vector3D g_MpS2;
+	
+	// magnetic field
+	private Vector3D f_uT;
+	
+	// GPS bias
+	private Vector3D x0_M;
+	
+	// accelerometer bias
+	private Vector3D a0_MpS2;
+	
+	// gyroscope bias
+	private Vector3D omega0_RADpS;
+	
+	// magnetometer bias
+	private Vector3D f0_uT;
+	
+	// system error
+	private RealMatrix P;
+	
+	// process error
+	private final RealMatrix Q;
+	
+	void update(long  tNew_nS) {
+		if(tNew_nS == t_nS) return;
+		
+		double dt_S = 1.0e-9*((double)(tNew_nS-t_nS));
+		
+		// update time
+		t_nS = tNew_nS;
+		
+		// update position
+		x_M = x_M.add(dt_S, v_MpS).add(0.5*dt_S*dt_S, a_MpS2);
+		
+		// update velocity
+		v_MpS = v_MpS.add(dt_S, a_MpS2);
+		
+		// update orientation
+		double dTheta_RAD = dt_S * omega_RADpS.getNorm();
+		theta = (new Rotation(omega_RADpS,dTheta_RAD)).applyTo(theta);
+		
+		// update error
+		RealMatrix F = getJacobian();
+		P = F.multiply(P).multiply(F.transpose()).add(Q.scalarMultiply(dt_S));
+	}
+	
+	RealMatrix getJacobian() {
+		
+	}
+	
+	void fuseGps(long timestamp, double lon_DEG, double lat_DEG, double alt_M, double acc_M) {
+		
+	}
+}
