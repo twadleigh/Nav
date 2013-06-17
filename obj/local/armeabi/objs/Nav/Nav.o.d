5bf31778c178cf219109b99d164d9c57f1954d9f
Tracy Wadleigh <twadleigh@.corp.arete.com>
2013-06-17 10:42:20 -0400

    Removing some generated files.

--- obj/local/armeabi/objs/Nav/Nav.o.d
+++ obj/local/armeabi/objs/Nav/Nav.o.d
@@ -1,6 +1,4 @@
-obj/local/armeabi/objs/Nav/Nav.o: jni/Nav.cpp jni/Nav.hpp \
+obj/local/armeabi/objs/nav/Nav.o: jni/Nav.cpp \
  /home/tracy/adt/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h
-
-jni/Nav.hpp:
 
 /home/tracy/adt/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h:
--- obj/local/armeabi/objs/nav/Nav.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs/nav/Nav.o: jni/Nav.cpp \
- /home/tracy/adt/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h
-
-/home/tracy/adt/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h:
